f2118afb22d3f596e6bf61a030f346467ba1bad0
Ennio Mara <enniomara@users.noreply.github.com>
2019-03-30 01:06:14 +0100

    Change main design to show buttons for assignment

--- app/src/main/java/com/example/mamn01_hello_sensor/MainActivity.java
+++ app/src/main/java/com/example/mamn01_hello_sensor/MainActivity.java
@@ -8,19 +8,17 @@ import android.view.View;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
-    public void sendMessage(View view) {
-        Intent intent = new Intent(this, DisplayMessageActivity.class);
-        EditText editText = (EditText) findViewById(R.id.editText);
-        String message = editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
+    public void openSensors(View view) {
+
+    }
+
+    public void openAccelerometers(View view) {
+
     }
 }
